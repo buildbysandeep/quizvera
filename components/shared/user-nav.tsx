1cff32acc3e05623bd895a1cbaf2381813dce810
sandeep k. <buildbysandeep@gmail.com>
2025-12-30 17:19:21 +0530

    fix: update sign-out redirect URL based on environment

--- components/shared/user-nav.tsx
+++ components/shared/user-nav.tsx
@@ -52,7 +52,7 @@ export function UserNav({ session }: { session: Session }) {
         <DropdownMenuItem
           onClick={() => {
             signOut({
-              redirectTo: "/",
+              redirectTo: process.env.NODE_ENV === "production" ? `${process.env.NEXT_PUBLIC_APP_URL}/sign-in` : "/sign-in",
             });
           }}
           className="cursor-pointer"
